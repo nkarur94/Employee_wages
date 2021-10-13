776b3cba9ca30a4d2481b8cdbf822d8177cfee7d
nithunkarur <rknithun@gmail.com>
2021-10-13 18:40:13 +0530

    welcome note

---
+++ employee_wages.java
@@ -0,0 +1,7 @@
+public class employee_wages{
+	public static void main(String[] args)
+	{
+		System.out.println("welcome to employee wage computational program");
+		System.out.println("------------------------------------------------");
+	}
+}
