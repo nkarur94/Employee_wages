c5a83cbe3b0096e1b6237e0e58e5e2e1d4896cdc
nithunkarur <rknithun@gmail.com>
2021-10-13 19:13:35 +0530

    Added: Daily wage calculation

--- employee_wages.java
+++ employee_wages.java
@@ -7,13 +7,22 @@ public class employee_wages{
                         System.out.println("employee is present");
                 }
                 else
-                        System.out.println("employee is absent");
+        		System.out.println("employee is absent");
 	}
+	public void dailyWages(){
+                int perHourWage=20;
+                int dailyWages=0;
+		int totalHour=8;
+		dailyWages=perHourWage*totalHour;
+                System.out.println("daily wages--"+dailyWages);
+	}
+
 	public static void main(String[] args)
 	{
 		System.out.println("welcome to employee wage computational program");
 		System.out.println("------------------------------------------------");
 		employee_wages ew1=new employee_wages();
 		ew1.presentOrAbsent();
+		ew1.dailyWages();
 	}
 }
