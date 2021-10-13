f9811f9723ce351b4ea3a8f0259fb0cef64d261b
nithunkarur <rknithun@gmail.com>
2021-10-13 19:02:25 +0530

    Added: Employee attendence

--- employee_wages.java
+++ employee_wages.java
@@ -1,7 +1,19 @@
+import java.util.Random;
 public class employee_wages{
+	public void presentOrAbsent(){
+                Random r1=new Random();
+                int attendenceTrack=r1.nextInt(2);
+                if(attendenceTrack==1){
+                        System.out.println("employee is present");
+                }
+                else
+                        System.out.println("employee is absent");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("welcome to employee wage computational program");
 		System.out.println("------------------------------------------------");
+		employee_wages ew1=new employee_wages();
+		ew1.presentOrAbsent();
 	}
 }
