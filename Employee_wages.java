9a5b172363d2150b97e70f737b3ba129a498c220
nithunkarur <rknithun@gmail.com>
2021-10-18 16:15:13 +0530

    Changed the code as per the class and methods for each operations

--- Employee_wages.java
+++ Employee_wages.java
@@ -1,14 +1,19 @@
 import java.util.Random;
-public class employee_wages{
-	public void presentOrAbsent(){
+public class Employee_wages{
+	
+	public void presentOrAbsent()
+	{
                 Random r1=new Random();
                 int attendenceTrack=r1.nextInt(2);
-                if(attendenceTrack==1){
+                
+		if(attendenceTrack==1)
+		{
                         System.out.println("employee is present");
                 }
                 else
         		System.out.println("employee is absent");
 	}
+	
 	public void WagesCal(){
                 int perHourWage=20;
                 int dailyWages=0;
@@ -16,7 +21,9 @@ public class employee_wages{
 		int monthWage=0;
 		Random r2=new Random();
 		int empCheck=r2.nextInt(2);
-		switch(empCheck){
+		
+		switch(empCheck)
+		{
 			case 0:
 				totalHour=4;
 				dailyWages=perHourWage*totalHour;
@@ -37,26 +44,36 @@ public class employee_wages{
 
 		}
 	}
-	public void conditionclac(){
+
+	public void conditionclac()
+	{
                 int count=0;
                 int no_days=31;
-                for(int i=0; i<=no_days; i++){
+                
+		for(int i=0; i<=no_days; i++) 
+		{
                         Random r3=new Random();
                         int dailyCheck=r3.nextInt(2);
-                                if(dailyCheck==1){
-                                        count++;
-                                        }
+                
+			if(dailyCheck==1)
+			{
+                                        count++;  
+		 	}
                 }
+
                 System.out.println("no of days-"+count);
                 int totalWorkingHrs=count*8;
-	                if (count<20 && totalWorkingHrs<100){
+	         
+	 	if (count<20 && totalWorkingHrs<100)
+		{
                         int MonthlyWages=count*500;
                         System.out.println("no of days  employee prsent in the month--"+count);
                         System.out.println("salary as per attendence--$"+MonthlyWages);
-                        }
-                        else{
+                 }
+		else
+		{
                         System.out.println("work excided");
-                        }
+                }
         }
 
 
@@ -64,7 +81,7 @@ public class employee_wages{
 	{
 		System.out.println("welcome to employee wage computational program");
 		System.out.println("------------------------------------------------");
-		employee_wages ew1=new employee_wages();
+		Employee_wages ew1=new Employee_wages();
 		ew1.presentOrAbsent();
 		ew1.WagesCal();
 		ew1.conditionclac();
